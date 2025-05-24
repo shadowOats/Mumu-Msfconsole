2c9fecbb5a9ba13592b18c0b8aa3c0f06446c12e
Oats <2765653855@qq.com>
2025-05-25 03:46:11 +0800

    没啥事，更新着玩~

--- push.bat
+++ push.bat
@@ -1,6 +1,17 @@
 chcp 65001
 @echo off
+
+echo 设置远程仓库为 shadowOats/Mumu-Msfconsole...
+git remote set-url origin https://github.com/shadowOats/Mumu-Msfconsole.git
+
+echo 添加更改...
 git add .
+
+echo 提交更改...
 git commit -m "没啥事，更新着玩~"
-git push
+
+echo 推送到 GitHub...
+git push origin main
+
+echo ✅ 推送完成！
 pause
